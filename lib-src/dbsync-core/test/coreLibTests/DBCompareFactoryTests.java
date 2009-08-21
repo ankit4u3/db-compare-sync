7ed3099b210c18a5dbee4fe39f9ccf9fbc8e2824
Frank Villasenor <Frank@TheOpenSourceU.com>
2009-08-21 11:58:27 -0500

    Theory work with Java. There may be too big of a learning curve to work with Java efficently. Considering Mono now.

---
+++ lib-src/dbsync-core/test/coreLibTests/DBCompareFactoryTests.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import org.junit;
+
+package coreLibTests;
+
+
+/**
+ *
+ * @author Frank
+ */
+ public class DBCompareFactoryTests
+{
+    
+}
