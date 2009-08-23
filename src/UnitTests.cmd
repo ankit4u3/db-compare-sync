54e02d3b46fbc3594597a1c6e517149dd902f502
Frank Villasenor <Frank@TheOpenSourceU.com>
2009-08-22 20:57:01 -0500

    Proper comments.

--- src/UnitTests.cmd
+++ src/UnitTests.cmd
@@ -1,7 +1,7 @@
-# Example Script to run unit tests.
-# but we need to find a way to make
-# the paths relative.
-#
+Rem - Example Script to run unit tests.
+Rem - but we need to find a way to make
+Rem - the paths relative.
+Rem
 
 Gallio.Echo /hint-directory:\SharedBin\ C:\Users\Frank\Desktop\db-compare-sync\src\DbCompareSync.core.UnitTests\bin\Debug\DbCompareSync.core.UnitTests.dll
 
