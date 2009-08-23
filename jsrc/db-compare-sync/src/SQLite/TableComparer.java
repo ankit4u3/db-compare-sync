013308849fb3742172450096f76bbb05a1a3f260
Frank Villasenor <Frank@TheOpenSourceU.com>
2009-08-23 15:17:23 -0500

    created a java version of the project. Not sure if java or CLR will be kept but for now both exist.

---
+++ jsrc/db-compare-sync/src/SQLite/TableComparer.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
+package SQLite;
+
+import core.*;
+
+/**
+ *
+ * @author FrankV
+ */
+public class TableComparer implements ITableComparer
+{
+    
+}
---
+++ jsrc/db-compare-sync/test/SQLite/SQLiteDBCompareTests.java
@@ -0,0 +1,51 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package SQLite;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author FrankV
+ */
+public class SQLiteDBCompareTests {
+
+    public SQLiteDBCompareTests() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+    @Test
+    public void TestDBCompare()
+    {
+        //Not sure what to test yet; need design.
+    }
+
+}
