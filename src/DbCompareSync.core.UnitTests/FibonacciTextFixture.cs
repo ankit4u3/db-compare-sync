e97c1903dbf6ae8ab09ee7d5cbf9e6bb33cabb77
Frank Villasenor <Frank@TheOpenSourceU.com>
2009-08-22 20:33:36 -0500

    Exploring Gallio. This commit is just an example unit test (that fails).

---
+++ src/DbCompareSync.core.UnitTests/FibonacciTextFixture.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Gallio.Framework;
+using MbUnit.Framework;
+using MbUnit.Framework.ContractVerifiers;
+
+namespace DbCompareSync.core.UnitTests
+{
+  [TestFixture]
+  public class FibonacciTextFixture
+  {
+    [Test]
+    public void FibonacciOfNumberGreaterThanOne()
+    {
+      int result = Fibonacci.Calculate(6);
+      Assert.AreEqual(result, 8);
+
+    }
+  }
+}
---
+++ src/DbCompareSync.core/Fibonacci.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DbCompareSync.core
+{
+  public class Fibonacci
+  {
+    public static int Calculate(int x)
+    {
+      if (x <= 0)
+      {
+        return 0;
+      }
+
+      return Fibonacci.Calculate(x - 1) + Fibonacci.Calculate(x - 2);
+    }
+
+  }
+}
