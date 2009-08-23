7875eb0b23751748a7c98b6b7a701efcf93a94b3
Frank Villasenor <Frank@TheOpenSourceU.com>
2009-08-22 20:53:13 -0500

    Example Unit Test Script file.

---
+++ src/UnitTests.cmd
@@ -0,0 +1,7 @@
+# Example Script to run unit tests.
+# but we need to find a way to make
+# the paths relative.
+#
+
+Gallio.Echo /hint-directory:\SharedBin\ C:\Users\Frank\Desktop\db-compare-sync\src\DbCompareSync.core.UnitTests\bin\Debug\DbCompareSync.core.UnitTests.dll
+
