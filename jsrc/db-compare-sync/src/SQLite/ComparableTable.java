8286e68b252481c964e65bc9bbe14fd563bf0315
Frank Villasenor <Frank@TheOpenSourceU.com>
2009-08-25 21:24:09 -0500

    Added the SQLite Comparable*.java files.

---
+++ jsrc/db-compare-sync/src/SQLite/ComparableTable.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package SQLite;
+
+import core.*;
+
+/**
+ *
+ * @author Frank
+ */
+public class ComparableTable extends core.ComparableTable
+{
+
+}
