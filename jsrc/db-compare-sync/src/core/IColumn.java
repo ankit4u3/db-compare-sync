7cf7c207ee35f010686e83fac50371af520145a7
Frank Villasenor <Frank@TheOpenSourceU.com>
2009-08-26 22:10:19 -0500

    Documentation for the object.

--- jsrc/db-compare-sync/src/core/IColumn.java
+++ jsrc/db-compare-sync/src/core/IColumn.java
@@ -6,17 +6,52 @@
 package core;
 
 /**
- *
+ * Represents a column. The implemention should be
+ * unique to the database package.
  * @author FrankV
  */
-public interface IColumn {
+public interface IColumn
+{
 
+    /**
+     * Should return the column name this object represents
+     * @return the column name
+     */
     String getColumnName();
 
-    String getDataType();
-
+    /**
+     * Set the name of the column
+     * @param psValue value to set.
+     * @throws Exception
+     */
     void setColumnName(String psValue) throws Exception;
 
+    /**
+     * Returns the data type
+     * @return string, the data type
+     */
+    String getDataType();
+
+    /**
+     * Set the data type for the column.
+     * @param psValue The new value.
+     * @throws Exception
+     */
     void setDataType(String psValue) throws Exception;
 
+
+    /**
+     * Returns the column position within the
+     * database.
+     * @return the order as an int.
+     */
+    int getOrdinal();
+
+    
+    /**
+     * Set the ordinal value
+     * @param psValue The position
+     * @throws Exception
+     */
+    void setOrdinal( int psValue ) throws Exception;
 }
