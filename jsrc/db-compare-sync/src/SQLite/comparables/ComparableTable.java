a753c212a36c86c6bcc9303fecca09f0e368a665
Frank Villasenor <Frank@TheOpenSourceU.com>
2009-08-26 21:47:53 -0500

    Implemented interfaces (with place holders that throw exceptions) for the Comparable objects.

--- jsrc/db-compare-sync/src/SQLite/comparables/ComparableTable.java
+++ jsrc/db-compare-sync/src/SQLite/comparables/ComparableTable.java
@@ -1,12 +1,41 @@
 
 package SQLite.comparables;
 
+import core.IColumn;
+import core.IComparableTable;
+import java.util.ArrayList;
+
 
 /**
  *
  * @author Frank
  */
-public class ComparableTable extends core.ComparableTable
+public class ComparableTable implements core.IComparableTable
 {
+
+    public boolean compare(IComparableTable CompareTo)
+    {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public ArrayList<IColumn> getFieldList()
+    {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void setFieldList(ArrayList<IColumn> psValue)
+    {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public String getTableName()
+    {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void setTableName(String psTableName)
+    {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 
 }
