31528acbb1d20d2607872882581bec56ae183439
Frank Villasenor <Frank@TheOpenSourceU.com>
2009-08-27 20:50:22 -0500

    Efforts on ComparableTable and ComparableDB.

---
+++ jsrc/db-compare-sync/test/SQLiteUnitTests/ComparableTableUnitTest.java
@@ -0,0 +1,101 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package SQLiteUnitTests;
+
+
+import SQLite.*;
+import SQLite.comparables.*;
+import core.Comparables.*;
+import core.*;
+
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import java.sql.*;
+import java.util.*;
+
+/**
+ *
+ * @author FrankV
+ */
+public class ComparableTableUnitTest
+{
+    private SQLiteUnitTestUtility _testUtility;
+    private Connection _conn;
+
+    public ComparableTableUnitTest()
+    {
+        this._testUtility = new SQLiteUnitTestUtility();
+        _conn = null;
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception
+    {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception
+    {
+    }
+
+    @Before
+    public void setUp()
+    {
+        
+    }
+
+    @After
+    public void tearDown()
+    {
+    }
+
+
+    /**
+     * Some basic tests for FieldList.
+     * @throws Exception
+     */
+     @Test
+     public void test_FieldList() throws Exception
+     {
+         IComparableTable _table = new ComparableTable();
+
+         ArrayList<IColumn> _fields = new ArrayList<IColumn>();
+
+         IColumn _col = new SQLiteColumn();
+         _col.setColumnName("TestCol");
+         _col.setDataType("INTEGER");
+         _col.setOrdinal(0);
+         _fields.add(_col);
+
+         _col = new SQLiteColumn();
+         _col.setColumnName("TestCol2");
+         _col.setDataType("TEXT");
+         _col.setOrdinal(1);
+         _fields.add(_col);
+
+         _table.setFieldList(_fields);
+
+         assertSame(_fields, _table.getFieldList());
+
+         assertEquals(2, _table.getFieldList().size());
+     }
+
+     @Test
+     public void test_FieldList_setNull()
+     {
+         IComparableTable _table = new ComparableTable();
+
+         _table.setFieldList(null);
+         assertNull(_table.getFieldList());
+     }
+
+}
