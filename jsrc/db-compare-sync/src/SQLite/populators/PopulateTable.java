50c032e1658951fe962f3ca38530d1c12ceea6bc
Frank Villasenor <Frank@TheOpenSourceU.com>
2009-08-27 21:26:51 -0500

    Created SQLite classes to implement the IPopulate* objects. Didn't do much with them though.

---
+++ jsrc/db-compare-sync/src/SQLite/populators/PopulateTable.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package SQLite.populators;
+
+import core.Comparables.IComparableTable;
+import core.populators.*;
+
+/**
+ *
+ * @author FrankV
+ */
+public class PopulateTable implements IPopulateTable
+{
+
+    public IComparableTable populate(IComparableTable pTable)
+    {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
---
+++ jsrc/db-compare-sync/src/SQLite/populators/PopulateView.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package SQLite.populators;
+
+import core.Comparables.IComparableView;
+import core.populators.*;
+
+/**
+ *
+ * @author FrankV
+ */
+public class PopulateView implements IPopulateView
+{
+
+    public IComparableView populate(IComparableView pView)
+    {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
